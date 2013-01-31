7ddd533f8d9ce115d4bbcf6a72bd830293f29f3e
Madhavi Padimi <padimi@Madhus-iMac.local>
2013-01-31 12:43:55 +0530

    commented

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Helloworld::Application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => 'gmail.com',
+  #:domain               => 'gmail.com',
   :user_name            => 'madhu.padimi1989@gmail.com',
   :password             => 'life2013',
   :authentication       => 'plain',
