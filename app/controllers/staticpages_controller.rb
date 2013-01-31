feec3dc8f5686461fec8aaa39dce535b0f5bff85
Madhavi Padimi <padimi@Madhus-iMac.local>
2013-01-31 19:50:44 +0530

    markers on map

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -2,31 +2,31 @@ class StaticpagesController < ApplicationController
   def home
   end
   def search
-  	
-     puts "*****************************************"
 
       require 'net/http'
 
       url = URI.parse('http://freegeoip.net/json/50.16.105.214')
+      #url = URI.parse('http://freegeoip.net/json/params[:search]')
       req = Net::HTTP::Get.new(url.path)
       res = Net::HTTP.start(url.host, url.port) {|http|
         http.request(req)
       }
 
-      puts res.body
-
       require 'json'
 
-      hash = JSON.parse(res.body)
-
-      puts hash
+      @hash = JSON.parse(res.body)
 
       puts "*****************************************"
 
-      hash.keys.sort.each do |key|
+      @hash.keys.sort.each do |key|
 
-      puts "#{key}----- #{hash[key]}"
+          puts "#{key}----- #{@hash[key]}"
 
-    end
+      end
+
+      respond_to do |format|
+        format.js #{render :content_type => 'text/javascript', :locals => {:hash => @hash}}
+      end
+
   end
 end
