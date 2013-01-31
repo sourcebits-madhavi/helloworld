de79061e65057d3e0825ef9a1f2f184738058a3b
Madhavi Padimi <padimi@Madhus-iMac.local>
2013-01-31 12:11:22 +0530

    smptp added in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,19 @@ Helloworld::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Don't care if the mailer can't send
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => 'madhu.padimi1989@gmail.com',
+  :password             => 'life2013',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end
